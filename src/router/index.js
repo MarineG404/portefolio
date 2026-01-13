ab8cd5fbcdad5cfb2fcc515eec0bc8ef1431e1fc
MarineG404 <marine.gonnord.pro@gmail.com>
2026-01-13 11:17:45 +0100

    ✨ Add scroll behavior to router for smooth navigation

--- src/router/index.js
+++ src/router/index.js
@@ -10,4 +10,16 @@ const routes = [
 export default createRouter({
 	history: createWebHistory(),
 	routes,
+	scrollBehavior(to, from, savedPosition) {
+		if (to.hash) {
+			return {
+				el: to.hash,
+				behavior: 'smooth',
+			}
+		}
+		if (savedPosition) {
+			return savedPosition
+		}
+		return { top: 0 }
+	}
 })
