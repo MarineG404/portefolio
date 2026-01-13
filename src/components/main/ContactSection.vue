b5da1ed2f2e1690fc14b4edfe45210f104ef68c1
MarineG404 <marine.gonnord.pro@gmail.com>
2026-01-13 13:28:41 +0100

    ✨ Add target and rel attributes to contact links for improved usability

--- src/components/main/ContactSection.vue
+++ src/components/main/ContactSection.vue
@@ -3,9 +3,9 @@
     <h2>Contactez-moi</h2>
     <p>Vous pouvez me contacter via les réseaux sociaux ou par email :</p>
     <div class="contact-links">
-      <a href="https://github.com/marineG404"><i class="ri-github-line"></i></a>
-      <a href="https://www.linkedin.com/in/marine-gonnord-7a1517234/"><i class="ri-linkedin-line"></i></a>
-      <a href="mailto:marine.gonnord.pro@gmail.com"><i class="ri-mail-line"></i></a>
+	<a href="https://github.com/marineG404" target="_blank" rel="noopener noreferrer"><i class="ri-github-line"></i></a>
+	<a href="https://www.linkedin.com/in/marine-gonnord-7a1517234/" target="_blank" rel="noopener noreferrer"><i class="ri-linkedin-line"></i></a>
+	<a href="mailto:marine.gonnord.pro@gmail.com" target="_blank" rel="noopener noreferrer"><i class="ri-mail-line"></i></a>
     </div>
   </section>
 </template>
